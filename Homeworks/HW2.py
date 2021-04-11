4fa2edecb0ffdd017eb9f47ac61070f04a11d63d
MuserrefOzkn <67440126+MuserrefOzkn@users.noreply.github.com>
2021-04-11 20:53:28 +0300

    Update HW2.py

--- Homeworks/HW2.py
+++ Homeworks/HW2.py
@@ -1,5 +1,16 @@
-#Explain your work
+#Get username and password values from the user
+# Check the values in an if statement and tell the user ir they were successful
 
 #Question 1
-for x in range(a):
-	print(a)
+dct = {"Username":"MÜŞERREF", "Password":"12345"}
+
+
+username = input("Username: ")
+password = input("Password: ")
+
+dct2 = {"Username":username.upper(), "Password":password}
+
+if dct == dct2:
+  print("Giriş başarılı")
+else:
+  print("Giriş başarısız. Tekrar deneyiniz")
