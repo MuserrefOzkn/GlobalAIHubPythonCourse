ba1a236a3da925bf578a99d3c93e111a898c9a51
MuserrefOzkn <67440126+MuserrefOzkn@users.noreply.github.com>
2021-04-11 20:42:27 +0300

    Update HW1.py

--- Homeworks/HW1.py
+++ Homeworks/HW1.py
@@ -1,5 +1,25 @@
 #Explain your work
 
 #Question 1
-for x in range(a):
-	print(a)
+mylist = []
+
+while True:
+  value = input("Please enter an integer value (press q for quit the number entering): ")
+  
+  if value == "q":
+    break
+  
+  mylist.append(value)
+
+if len(mylist) %2 != 0:
+  first_half = mylist[:(int(len(mylist2)/2))]
+  second_half = mylist[(int(len(mylist2)/2)):]
+    
+else:
+  first_half = mylist[:int(len(mylist)/2)]
+  second_half = mylist[int(len(mylist)/2):]
+
+first_half, second_half = second_half, first_half
+first_half.extend(second_half)
+
+print(first_half)
